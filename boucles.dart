b5bbf6c4851ac901f7329b890ad13558aa8d4cb2
Maxime <maxime.b2494@gmail.com>
2024-06-20 09:37:59 +0200

    exercice sur les boucles : boucle pour une liste

--- boucles.dart
+++ boucles.dart
@@ -4,6 +4,8 @@
 // Utilisez une boucle for pour parcourir chaque élément de la liste 
 // et les afficher à la console.
 
+import 'dart:math';
+
 void numberList() {
   // List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -34,20 +36,93 @@ void evenNumber() {
   }
 }
 
-// void race(){
-//   int position = 0;
-//   int arrival = 20;
+// Vous devez créer un programme en Dart qui utilise une boucle while pour 
+// simuler une course entre deux coureurs. Le programme doit afficher la 
+// position des coureurs à chaque itération de la boucle jusqu'à ce qu'un 
+// des coureurs atteigne la ligne d'arrivée.
+// Pour faire avancer les coureurs, vous utilserez un nombre aléatoire avec
+// Random().nextInt(4) + 1.
+
+void race(){
+  int runner1 = 0;
+  int runner2 = 0;
+  int arrival = 20;
   
-//   while(position < arrival){
+  while(
+    (runner1 < arrival) 
+    || 
+    (runner2 < arrival)
+    ){
+    runner1 += Random().nextInt(4) +1;
+    runner2 += Random().nextInt(4) +1;
 
-//   }
+    print("Position du coureur 1 : $runner1");
+    print("Position du coureur 2 : $runner2");
 
-// }
+    if(runner1 > runner2){
+      print("Le joueur 1 est en tête");
+    } else {
+      print("Le joueur 2 est en tête");
+    }
+  }
 
+  print("Fin de la course");
+}
+
+void raceCorrection() {
+   // Initial position of runners
+  int runner1Pos = 0;
+  int runner2Pos = 0;
+ 
+  // Length of the track
+  int trackLength = 100;
+ 
+  // Loop until a runner crosses the finish line
+  while (runner1Pos < trackLength && runner2Pos < trackLength) {
+    // Increment the position of each runner
+    runner1Pos += Random().nextInt(4) + 1;
+    runner2Pos += Random().nextInt(4) + 1;
+ 
+    // Display the current positions of the runners
+    print('Runner 1: ${'*' * (trackLength - runner1Pos)}');
+    print('Runner 2: ${'*' * (trackLength - runner2Pos)}\n');
+  }
+ 
+  // Determine the winner
+  if (runner1Pos >= trackLength) {
+    print('Runner 1 wins!');
+  } else {
+    print('Runner 2 wins!');
+  }
+}
+
+// Créez une boucle qui génère une séquence de nombres suivant une règle simple :
+// chaque nombre de la séquence est la somme des deux nombres précédents.
+// La boucle do while doit afficher la séquence jusqu'à ce que la somme 
+// des nombres de la séquence dépasse un nombre maximum déterminé au préalable.
+
+void sumSequence() {
+  int maxNumber = 100;
+  int num1 = 0;
+  int num2 = 1;
+  int sum = 0;
+  do {
+    sum += num1 + num2;
+    print("Somme : + $num1 + $num2 = $sum");
+    num1 = num2;
+    num2 = sum;
+  } while (sum <= maxNumber);
+}
 
 void main(List<String> args) {
   
   // numberList();
   // numberListCorrection();
-  evenNumber();
+
+  // evenNumber();
+  
+  // race();
+  // raceCorrection();
+
+  sumSequence();
 }
